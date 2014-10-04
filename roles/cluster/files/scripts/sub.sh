f9362810e945c465a5ba4cc22177ad01a701824f
Christian Kniep <christian@qnib.org>
2014-10-04 12:01:30 +0200

    update sub script to submit to venus-partition

--- roles/cluster/files/scripts/sub.sh
+++ roles/cluster/files/scripts/sub.sh
@@ -3,33 +3,53 @@
 DIRNAME=$(dirname ${0})
 MPI_VER=$(echo ${DIRNAME}|egrep -o "omp\-(sys|[0-9\.]+)")
 DIR_TUP=$(echo ${DIRNAME}|egrep -o "[0-9\.]+"|xargs)
-OS_VER=$(echo ${DIR_TUP}|awk '{print $1}')
-OS_BASE=$(echo ${DIRNAME}|egrep -o "(centos|fedora|ubuntu)")
+DIR_BASE=$(echo ${DIRNAME}|egrep -o "(centos|fedora|ubuntu|venus)")
 MPI_RELEASE="NONE"
 
-
-if [ "${MPI_VER}" = "omp-sys" ];then
-    PROB_SIZE=$(echo ${DIR_TUP}|awk '{print $2}')
-    JOB_TIME=$(echo ${DIR_TUP}|awk '{print $3}')
+if [ "${DIR_BASE}" = "venus" ];then
+    OS_BASE="centos"
+    OS_VER=7
+    if [ "${MPI_VER}" = "omp-sys" ];then
+        PROB_SIZE=$(echo ${DIR_TUP}|awk '{print $1}')
+        JOB_TIME=$(echo ${DIR_TUP}|awk '{print $2}')
+    else
+        MPI_RELEASE=$(echo ${DIR_TUP}|awk '{print $1}')
+        PROB_SIZE=$(echo ${DIR_TUP}|awk '{print $2}')
+        JOB_TIME=$(echo ${DIR_TUP}|awk '{print $3}')
+    fi
+    
 else
-    MPI_RELEASE=$(echo ${DIR_TUP}|awk '{print $2}')
-    PROB_SIZE=$(echo ${DIR_TUP}|awk '{print $3}')
-    JOB_TIME=$(echo ${DIR_TUP}|awk '{print $4}')
+    OS_BASE="${DIR_BASE}"
+    OS_VER=$(echo ${DIR_TUP}|awk '{print $1}')
+    if [ "${MPI_VER}" = "omp-sys" ];then
+        PROB_SIZE=$(echo ${DIR_TUP}|awk '{print $2}')
+        JOB_TIME=$(echo ${DIR_TUP}|awk '{print $3}')
+    else
+        MPI_RELEASE=$(echo ${DIR_TUP}|awk '{print $2}')
+        PROB_SIZE=$(echo ${DIR_TUP}|awk '{print $3}')
+        JOB_TIME=$(echo ${DIR_TUP}|awk '{print $4}')
+    fi
 fi
-PROC=8
 
-if [ "X${OS_BASE}" = "Xcentos" ];then
+
+if [ "X${DIR_BASE}" = "Xvenus" ];then
+    SLURM_PARTITION="venus"
+elif [ "X${DIR_BASE}" = "Xcentos" ];then
     SLURM_PARTITION="cos${OS_VER}"
-elif [ "X${OS_BASE}" = "Xubuntu" ];then
+elif [ "X${DIR_BASE}" = "Xubuntu" ];then
     SLURM_PARTITION="u${OS_VER}"
 fi
-
+PROC=8
 for x in $*;do
    key=$(echo $x|awk -F\: '{print $1}')
    val=$(echo $x|awk -F\: '{print $2}')
    declare $key=$val
 done
-JOB_PATH=${HOME}/${OS_BASE}/${OS_VER}/${PROB_SIZE}/${JOB_TIME}/$(date +"%F_%H-%M-%S")
+if [ "X${DIR_BASE}" = "Xvenus" ];then
+    JOB_PATH=${HOME}/job_results/${DIR_BASE}/${MPI_VER}/${PROB_SIZE}/${JOB_TIME}/$(date +"%F_%H-%M-%S")
+else
+    JOB_PATH=${HOME}/job_results/${OS_BASE}/${OS_VER}/${MPI_VER}/${PROB_SIZE}/${JOB_TIME}/$(date +"%F_%H-%M-%S")
+fi
 
 #echo """
 #MPI_VER=${MPI_VER}
@@ -41,7 +61,6 @@ JOB_PATH=${HOME}/${OS_BASE}/${OS_VER}/${PROB_SIZE}/${JOB_TIME}/$(date +"%F_%H-%M
 #SLURM_PARTITION=${SLURM_PARTITION}
 #JOB_PATH=${JOB_PATH}
 #"""
-
 
 mkdir -p ${JOB_PATH}
 cat << EOF > ${JOB_PATH}/job.cfg
@@ -70,4 +89,4 @@ if [ "X${after}" != "X" ];then
    SLURM_AFTER=" --dependency=afterany:${after}"
 fi
 echo "sbatch -N ${PROC} ${SLURM_AFTER} ${JOB_PATH}/run.sh"
-#sbatch -N ${PROC} ${SLURM_AFTER} ${JOB_PATH}/run.sh
+sbatch -N ${PROC} ${SLURM_AFTER} ${JOB_PATH}/run.sh
